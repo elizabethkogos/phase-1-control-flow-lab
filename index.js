deaea7e047c6d7dfca2a3a33e03f7dee889164cc
elizabethkogos <lizkogos@gmail.com>
2022-08-16 05:09:37 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 function scuberGreetingForFeet(){
+  let result
+  if (scuberGreetingForFeet <=400 ) {
+    result ='This one is on me!'
+    
+  } else if (scuberGreetingForFeet >2000) {
+    result = 'I will gladly take your thirty bucks.'
+
+     else (scuberGreetingForFeet >2500) 
+     result = 'No can do.'
   // Write your code here!
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+}let city= NYC
+
+if (city=NYC) {
+  result='Ok, sounds good.'
+  else  result = 'No go.'
+
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
-}
+} let switchOnCharmFromTip =generous
+ 
+ if (switchOnCharmFromTip = generous) {
+  result= 'Thank you.'
+  
+ } else { result ="Bye." 
+  
+ }
